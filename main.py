69af1c2d0b1b84fc36d766771dec553ceae51ee9
lpshkn <kofffsh@gmail.com>
2021-02-24 01:55:38 +0300

    Update the main module: run the program in asynchronous mode

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from TikTokApi import TikTokApi
+import asyncio
+from tiktokinformer.informer.tiktokinformer import TikTokInformer
 from tiktokinformer.utils import get_top_users
+from tiktokinformer.database.db import Database
 
 
-def main():
+async def main():
+    db = Database.connect(host='localhost', port='5432',
+                          user='postgres', password='KPN6trvEBaguvix123852951',
+                          database='tiktokinformer')
+
     names = get_top_users(10)
+
+    informer = TikTokInformer(names, database=db)
+    await informer.run()
 
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
