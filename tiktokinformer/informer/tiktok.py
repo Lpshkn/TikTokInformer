0504198749131d5f83ce6049dbd4bcbdc5be29db
lpshkn <kofffsh@gmail.com>
2021-02-23 23:04:13 +0300

    Add module representing a row of the tiktoks table in the database

---
+++ tiktokinformer/informer/tiktok.py
@@ -0,0 +1,22 @@
+class Tiktok:
+    def __init__(self, tiktok_dict: dict):
+        self._id = tiktok_dict['id']
+        self._desc = tiktok_dict['desc']
+        self._time = tiktok_dict['createTime']
+        self._user_id = tiktok_dict['author']['uniqueId']
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def desc(self):
+        return self._desc
+
+    @property
+    def time(self):
+        return self._time
+
+    @property
+    def user_id(self):
+        return self._user_id
