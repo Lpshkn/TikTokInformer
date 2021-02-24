5a513022784909af2ef608b0ee8d48e676521f8d
lpshkn <kofffsh@gmail.com>
2021-02-24 23:25:47 +0300

    Add logging and initializing a dictionary of timestamps
    This dictionary allows to check appearance of new videos of the specified list of profiles.

--- tiktokinformer/informer/tiktokinformer.py
+++ tiktokinformer/informer/tiktokinformer.py
@@ -1,9 +1,13 @@
 import asyncio
+import logging
 from TikTokApi import TikTokApi
 from tiktokinformer.utils import get_sublists
 from tiktokinformer.informer.user import User
 from tiktokinformer.informer.tiktok import Tiktok
 from tiktokinformer.database.db import Database
+
+logging.basicConfig(format='[%(asctime)s]: %(message)s\n',
+                    level=logging.INFO)
 
 
 class TikTokInformer:
@@ -11,6 +15,8 @@ class TikTokInformer:
     timeout = 300
     # Count of concurrent tasks
     count_tasks = 5
+    # Dict of {username: timestamp_of_the_last_video}
+    last_timestamps = {}
 
     def __init__(self, names: list, database: Database):
         self.database = database
@@ -21,11 +27,21 @@ class TikTokInformer:
         """
         Runs a loop that creates tasks and waits when they will be finished.
         """
+        self._init_timestamps()
+
         names_lists = get_sublists(self.names, self.count_tasks)
 
         while True:
             tasks = [asyncio.create_task(self._load_profiles(names_list)) for names_list in names_lists]
-            await asyncio.gather(tasks)
+            await asyncio.gather(*tasks)
+
+    def _init_timestamps(self):
+        """
+        Gets from the database the timestamp of the last video of each user.
+        """
+        for name in self.names:
+            timestamp = self.database.get_last_timestamp(name)
+            self.last_timestamps[name] = timestamp
 
     async def _load_profiles(self, names: list):
         """
