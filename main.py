10f6aef319d5afddd3d26904fd88ebbd47ff38be
lpshkn <kofffsh@gmail.com>
2021-02-25 01:06:56 +0300

    Add Dockerfile and getting environment values in the main module

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import asyncio
+import os
 from tiktokinformer.informer.tiktokinformer import TikTokInformer
 from tiktokinformer.utils import get_top_users
 from tiktokinformer.database.db import Database
 
+PG_HOST = os.getenv('PG_HOST')
+PG_PORT = os.getenv('PG_PORT')
+PG_NAME = os.getenv('PG_NAME')
+PG_USER = os.getenv('PG_USER')
+PG_PASS = os.getenv('PG_PASS')
+
 
 async def main():
-    db = Database.connect(host='localhost', port='5432',
-                          user='postgres', password='KPN6trvEBaguvix123852951',
-                          database='tiktokinformer')
+    db = Database.connect(host=PG_HOST, port=PG_PORT,
+                          user=PG_USER, password=PG_PASS,
+                          database=PG_NAME)
 
     names = get_top_users(10)
 
