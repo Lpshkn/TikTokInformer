d733e5b9336d7d8f355ca866440dac5c95896a7b
lpshkn <kofffsh@gmail.com>
2021-02-24 23:31:44 +0300

    Add converting the int type of the passed timestamp to the datetime type

--- tiktokinformer/informer/tiktok.py
+++ tiktokinformer/informer/tiktok.py
@@ -1,8 +1,11 @@
+from datetime import datetime as dt
+
+
 class Tiktok:
     def __init__(self, tiktok_dict: dict):
         self._id = tiktok_dict['id']
         self._desc = tiktok_dict['desc']
-        self._time = tiktok_dict['createTime']
+        self._time = dt.fromtimestamp(tiktok_dict['createTime'])
         self._user_id = tiktok_dict['author']['uniqueId']
 
     @property
