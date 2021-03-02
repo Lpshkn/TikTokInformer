13e33db0ec7ce427368cdf1062765f99c887d565
lpshkn <kofffsh@gmail.com>
2021-03-03 02:29:04 +0300

    Add the bot to the main function and run it and the informer concurrently

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import os
 from tiktokinformer.informer.tiktokinformer import TikTokInformer
 from tiktokinformer.utils import get_top_users
 from tiktokinformer.database.db import Database
+from tiktokinformer.bot.bot import TikTokInformerBot
 
 PG_HOST = os.getenv('PG_HOST')
 PG_PORT = os.getenv('PG_PORT')
 PG_NAME = os.getenv('PG_NAME')
 PG_USER = os.getenv('PG_USER')
 PG_PASS = os.getenv('PG_PASS')
+TOKEN = os.getenv('TOKEN')
 
 
 async def main():
@@ -18,8 +20,13 @@ async def main():
 
     names = get_top_users(10)
 
-    informer = TikTokInformer(names, database=db)
-    await informer.run()
+    bot = TikTokInformerBot(token=TOKEN, database=db)
+    informer = TikTokInformer(names=names, database=db)
+
+    await asyncio.gather(
+        asyncio.create_task(informer.run()),
+        asyncio.create_task(bot.run())
+    )
 
 
 if __name__ == '__main__':
