bee5d3f35262e7cb780e675a5c8b17dd420cfaaf
lpshkn <kofffsh@gmail.com>
2021-03-03 01:20:03 +0300

    Add tables for work of a bot
    This code was taken from the Lpshkn/BirthdayTelegramBot

--- tiktokinformer/database/db.py
+++ tiktokinformer/database/db.py
@@ -1,5 +1,7 @@
 import psycopg2
 import logging
+from psycopg2 import sql
+from collections import defaultdict
 from tiktokinformer.informer.user import User
 from tiktokinformer.informer.tiktok import Tiktok
 from datetime import datetime as dt
@@ -36,32 +38,43 @@ class Database:
         """
         Creates tables if it weren't created.
         """
-        sql_query = """
-                    CREATE TABLE IF NOT EXISTS users
-                    (
-                        unique_id TEXT PRIMARY KEY,
-                        nickname TEXT NOT NULL, 
-                        followers_cnt INTEGER NOT NULL,
-                        following_cnt INTEGER NOT NULL,
-                        heart_cnt INTEGER NOT NULL,
-                        video_cnt INTEGER NOT NULL
-                    );
+        with self.connection.cursor() as cur:
+            cur.execute("CREATE TABLE IF NOT EXISTS users ("
+                        "unique_id TEXT PRIMARY KEY,"
+                        "nickname TEXT NOT NULL,"
+                        "followers_cnt INTEGER NOT NULL,"
+                        "following_cnt INTEGER NOT NULL,"
+                        "heart_cnt INTEGER NOT NULL,"
+                        "video_cnt INTEGER NOT NULL);")
                     
-                    CREATE TABLE IF NOT EXISTS tiktoks
-                    (
-                        id BIGINT PRIMARY KEY,
-                        user_id TEXT,
-                        description TEXT NOT NULL,
-                        time TIMESTAMP NOT NULL,
+            cur.execute("CREATE TABLE IF NOT EXISTS tiktoks ("
+                        "id BIGINT PRIMARY KEY,"
+                        "user_id TEXT,"
+                        "description TEXT NOT NULL,"
+                        "time TIMESTAMP NOT NULL,"
+                        "CONSTRAINT fk_users FOREIGN KEY (user_id)"
+                        "REFERENCES users (unique_id)"
+                        "ON DELETE CASCADE"
+                        "ON UPDATE CASCADE);")
                     
-                        CONSTRAINT fk_users FOREIGN KEY (user_id)
-                            REFERENCES users (unique_id)
-                            ON DELETE CASCADE
-                            ON UPDATE CASCADE
-                    );
-                    """
-        cursor = self.connection.cursor()
-        cursor.execute(sql_query)
+            cur.execute("CREATE TABLE IF NOT EXISTS conversations ("
+                        "chat_id INTEGER PRIMARY KEY,"
+                        "main_menu_state INTEGER NULL);")
+
+            cur.execute("CREATE TABLE IF NOT EXISTS chats ("
+                        "chat_id integer CONSTRAINT chats_pkey PRIMARY KEY REFERENCES conversations "
+                        "ON DELETE CASCADE ON UPDATE CASCADE,"
+                        "title varchar(256),"
+                        "description varchar(256),"
+                        "photo varchar(1000)")
+
+            cur.execute("CREATE TABLE IF NOT EXISTS bot_users ("
+                        "user_id integer CONSTRAINT users_pkey PRIMARY KEY,"
+                        "chat_id integer REFERENCES conversations ON DELETE SET NULL ON UPDATE CASCADE,"
+                        "username varchar(32),"
+                        "first_name varchar(256),"
+                        "last_name varchar(256)")
+
         self.connection.commit()
 
     @property
@@ -145,3 +158,142 @@ class Database:
         timestamp = cursor.fetchone()[0]
 
         return timestamp if timestamp else dt.now()
+
+    def update_conversations(self, conversations: dict):
+        """
+        Method updates a conversation's state in the database.
+
+        :param conversations: a dictionary containing chat_id and conversation state
+        """
+
+        # Conversations is dict containing items like: "name_conversation_handler: {chat_id: state}"
+        # And it's necessary to swap these values to pass it into the updating data method
+        # Now it will be like: "chat_id: {name_conversation_handler: state}"
+        _conversations = defaultdict(dict, {})
+        for name, id_states in conversations.items():
+            for chat_id, state in id_states.items():
+                _conversations[chat_id[0]][name] = state
+        self.update_data('conversations', _conversations)
+
+    def get_conversations(self) -> dict:
+        """
+        Method gets conversation states from the database.
+        This method returns a dictionary consisting of a tuple and a state of the handler.
+        The tuple contains the chat_id. It returns a tuple because of the handler requires it and besides the chat_id,
+        the user_id and the message_id may be passed. But now it isn't required and just user_id will be sent.
+        This fact may be changed in the ConversationHandler constructor (parameters per_chat, per_user, per_message)
+
+        :return: dictionary
+        """
+
+        # It's necessary for the handler that the dictionary will contain the name of the handler like the key
+        # Therefore, it must be swapped
+        conversations = defaultdict(dict, {})
+        for chat_id, names_states in self.get_data('conversations').items():
+            for name, state in names_states.items():
+                conversations[name][(chat_id,)] = state
+        return conversations
+
+    def get_chat_data(self) -> dict:
+        """
+        Method to getting chat_data from the database.
+        Returns dictionary containing the chat_id and a list of arguments.
+
+        :return: dictionary
+        """
+        return self.get_data("chats")
+
+    def get_user_data(self) -> dict:
+        """
+        Method to getting user_data from the database.
+        Returns dictionary containing the user_id and a list of arguments.
+
+        :return: dictionary
+        """
+        return self.get_data("users")
+
+    def get_data(self, table_name: str) -> dict:
+        """
+        Method builds a query to get all data from the table of the database.
+
+        :param table_name: the name of a table
+        :return: dictionary containing id and a list of arguments
+        """
+        with self.connection.cursor() as cur:
+            query = sql.SQL("SELECT column_name FROM information_schema.columns WHERE table_name = {};").format(
+                sql.Literal(table_name))
+            cur.execute(query)
+
+            columns = [column[0] for column in cur.fetchall()]
+
+            if not columns:
+                logging.warning("The names of columns from the Chats table weren't received")
+                return {}
+
+            # Get all data and create a dictionary
+            query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
+            cur.execute(query)
+            data = cur.fetchall()
+
+            if data:
+                return {row[0]: {columns[i]: row[i] for i in range(1, len(columns))} for row in data}
+            else:
+                return {}
+
+    def update_data(self, table_name: str, data: dict):
+        """
+        Method builds a query to update all data of the table of the database.
+
+        :param table_name: the name of a table
+        :param data: dictionary containing ids and a list of arguments with it
+        """
+        if data:
+            # Iterate through all ids
+            for id, data_dict in data.items():
+                with self.connection.cursor() as cur:
+                    # Get all the names of the columns of the table
+                    query = sql.SQL("SELECT column_name FROM information_schema.columns WHERE table_name = {};").format(
+                        sql.Literal(table_name))
+                    cur.execute(query)
+                    columns = [column[0] for column in cur.fetchall() if column[0] in data[id] or 'id' in column[0]]
+
+                    # Build a list containing all values of this id
+                    values = [id]
+                    values.extend(data[id].get(column) for column in columns[1:])
+
+                    if not columns:
+                        logging.warning("The names of columns from the {} table weren't received".format(table_name))
+                        return {}
+
+                    # Build the SQL-string to use "UPSERT" function
+                    setting_columns = list(
+                        map(lambda x: sql.SQL("{0} = excluded.{0}").format(sql.Identifier(x)), columns[1:]))
+
+                    # Build a query
+                    query = sql.SQL("INSERT INTO {0}({1}) "
+                                    "VALUES ({2}) "
+                                    "ON CONFLICT ({3}) DO UPDATE SET "
+                                    "{4}").format(
+                        sql.Identifier(table_name),
+                        sql.SQL(',').join(map(sql.Identifier, columns)),
+                        sql.SQL(',').join(map(sql.Literal, values)),
+                        sql.Identifier(columns[0]),
+                        sql.SQL(',').join(setting_columns))
+                    cur.execute(query)
+
+    def update_chat_data(self, chat_data: dict):
+        """
+        Method updates the data of chats in the database (if it was changed).
+
+        :param chat_data: a new data
+        """
+        self.update_data('chats', chat_data)
+
+    def update_user_data(self, user_data: dict):
+        """
+        Method updates the data of users in the database (if it was changed).
+
+        :param user_data:
+        :return:
+        """
+        self.update_data('users', user_data)
